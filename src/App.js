3e8eae06865b6cfda0c4bd4d9eef546627b51e39
Facundo Palombo <facundo.palombo@gmail.com>
2019-09-04 16:46:24 -0300

    Add something like a guard in react with functional component and render props for user auth validation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react'
-import { GlobalStyle } from './styles/GlobalStyle'
-import { Logo } from './components/Logo'
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
-import { Home } from './pages/Home'
 import { Router } from '@reach/router'
-import { Detail } from './pages/Detail'
+
+import { GlobalStyle } from './styles/GlobalStyle'
+
+import { Logo } from './components/Logo'
 import { NavBar } from './components/NavBar'
 
+import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 export const App = () => {
   return (
     <>
       <GlobalStyle />
       <Logo />
-      {
-        (
-          <Router>
-            <Home path='/' />
-            <Home path='/pet/:id' />
-            <Detail path='/detail/:id' />
-          </Router>
-        )
-      }
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:id' />
+        <Detail path='/detail/:id' />
+      </Router>
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? (
+                <Router>
+                  <Favs path='/favs' />
+                  <User path='/user' />
+                </Router>
+              )
+              : (
+                <Router>
+                  <NotRegisteredUser path='/favs' />
+                  <NotRegisteredUser path='/user' />
+                </Router>
+              )
+        }
+      </UserLogged>
       <NavBar />
     </>
   )
